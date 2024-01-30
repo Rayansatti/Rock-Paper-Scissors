857912a26f2b9ae5bb470b7699b87decaeceb07c
rayansatti <rayansatti013@gmail.com>
2024-01-30 21:52:21 +1100

    playRound function
    compares between the result from getcomputerchoice and playerchoice and returns a value

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function getComputerChoice(max){
     
 }
 
-const array = ['Rock' , 'Paper', 'Scissors'];
+const array = ['rock' , 'paper', 'scissors'];
 
 const result = getComputerChoice(array);
 
@@ -17,6 +17,31 @@ console.log(result)
 
 //gets input from the user and converts it to lower case
 let playerSelection = prompt("Enter your choice!").toLowerCase();
+
+
+//plays round in the game 
+//it takes 2 arguments the player selection and the result from getcomputerchoice(array)
+//compares between them and return a value
+function playRound(playerSelection, result){
+        if (playerSelection === result){
+                return("tie");
+        }else if(playerSelection == "rock" && result == "paper"){
+                return("computer wins!");
+        }else if(playerSelection == "rock" && result == "scissors"){
+                return ("you win!");
+        }else if(playerSelection == "paper" && result == "rock"){
+                return ("you win!");
+        }else if(playerSelection == "paper" && result == "scissors"){
+                return ("computer win!");
+        }else if(playerSelection == "scissors" && result == "rock"){
+                return ("computer win!");
+        }else if(playerSelection == "scissors" && result == "paper"){
+                return ("you win!");
+        }
+
+}
+
+console.log(playRound(playerSelection, result))
 
 
 
